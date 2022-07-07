6e9d49702f6214a06add373836b3544ac37d4081
Animesh Kumar Shukla <56479786+animeshaks@users.noreply.github.com>
2022-07-08 01:13:30 +0530

    updated 010-TwoSum.cpp

--- 01.Arrays/010-TwoSum.cpp
+++ 01.Arrays/010-TwoSum.cpp
@@ -3,24 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int> &nums, int target){
-
-    //TLE
-    // vector<int> ans;
-    // sort(nums.begin(), nums.end());
-    // int i=0,j=nums.size()-1;
-    // while(i<j){
-    //     if(nums[i] + nums[j] == target){
-    //         ans.push_back(nums[i]);
-    //         ans.push_back(nums[j]);
-    //     }
-    // }
-    // return ans;
-
-
-    //Optimal - O(n)
+// If asked to return indexes
+vector<int> twoSum(vector<int> &nums, int target)
+{
     unordered_map<int, int> m;
-    for (int i = 0; i < nums.size(); i++){
+    for (int i = 0; i < nums.size(); i++)
+    {
         if (m.find(nums[i]) != m.end())
             return {m[nums[i]], i};
         else
@@ -29,7 +17,29 @@ vector<int> twoSum(vector<int> &nums, int target){
     return {};
 }
 
-int main(){
+// If asked to return values
+vector<int> twoSumII(vector<int> &nums, int target){
+    vector<int> ans;
+    sort(nums.begin(), nums.end());
+    int i = 0, j = nums.size() - 1;
+    while (i < j){
+        if (nums[i] + nums[j] < target){
+            i++;
+        }
+        else if (nums[i] + nums[j] > target){
+            j--;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return {};
+}
+
+int main()
+{
 
     return 0;
 }
